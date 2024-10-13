d7e0bb28b8619932a0cf5df933305207d275a5e3
Artem Trifonov <artemis.levitt@gmail.com>
2024-10-13 19:19:25 +0200

    Initial commit

---
+++ src/main/java/com/alevitt/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,13 @@
+package com.alevitt.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
